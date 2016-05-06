d1bc5130011389bfff66bccc335c1c47dbfa2cc7
Jack Sung <magicsung@gmail.com>
2016-05-06 18:03:58 +0800

    env setting

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+livereload = require('livereload');
+
+app.use('/', express.static(__dirname + '/views'));
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/image', express.static(__dirname + '/images'));
+
+
+
+http.listen(8080, "0.0.0.0", function(){
+  console.log('listening on *:8080');
+});
+
+server = livereload.createServer();
+
+server.watch([__dirname + "/views", __dirname + "/js", __dirname + "/css"]);
