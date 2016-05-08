1c45ba2d38a0562a8e081add3ddc2cc826886e2c
Jack Sung <magicsung@gmail.com>
2016-05-08 15:16:17 +0800

    test sub folder.

--- server.js
+++ server.js
@@ -7,8 +7,7 @@ app.use('/', express.static(__dirname + '/views'));
 app.use('/css', express.static(__dirname + '/css'));
 app.use('/js', express.static(__dirname + '/js'));
 app.use('/image', express.static(__dirname + '/images'));
-
-
+app.use('/daily-ui', express.static(__dirname + '/daily-ui'));
 
 http.listen(8080, "0.0.0.0", function(){
   console.log('listening on *:8080');
@@ -16,4 +15,4 @@ http.listen(8080, "0.0.0.0", function(){
 
 server = livereload.createServer();
 
-server.watch([__dirname + "/views", __dirname + "/js", __dirname + "/css"]);
+server.watch([__dirname + "/views", __dirname + "/js", __dirname + "/css", __dirname + "/daily-ui"]);
